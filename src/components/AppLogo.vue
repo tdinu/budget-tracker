acea4334948062f4a874506e570657b97bc26a16
tdinu <dinutitiana@yahoo.com>
2024-10-01 22:48:41 +0200

    added icons and app logo

---
+++ src/components/AppLogo.vue
@@ -0,0 +1,14 @@
+<template>
+  <a class="flex items-center gap-2">
+    <HandCoins class="stroke h-11 w-11 stroke-violet-500 stroke-[1.5] hidden md:block" />
+    <p
+      class="bg-gradient-to-r from-violet-400 to-blue-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent"
+    >
+      BudgetTracker
+    </p>
+  </a>
+</template>
+
+<script setup>
+import { HandCoins } from 'lucide-vue-next'
+</script>
